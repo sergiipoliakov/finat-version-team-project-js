9d7bb91df9ac0800b107ea4a0304d50ce23e52d9
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-12-17 20:35:47 +0200

    почти готово

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import "./sass/main.scss";
+import './sass/main.scss';
+
+import './js/authorization/login';
+import './js/authorization/logout';
+
+import './js/header/menuBtn';
+import './js/my-cabinet/main-myCabinet';
+
+import './js/search-form/search-form-open';
+import './js/search-form/searchApiServis';
+
+import './js/add-modal-logic/add-modal-send';
+import './js/add-modal-logic/add-modal-open-close';
